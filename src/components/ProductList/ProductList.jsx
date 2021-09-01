4d555688da768dcf379cc8f05c31d81730231251
puneetsingh99 <99singhpuneet99@gmail.com>
2021-09-01 11:20:33 +0530

    refactor: change vars based on change context vars

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,19 +2,20 @@ import { ProductCard } from "../";
 import "./product-list-styles.css";
 import { useProducts } from "../../contexts/ProductContext/ProductContext";
 import { Loader } from "../Loader/Loader";
+import { notify } from "../../utils";
 
 export const ProductList = () => {
-  const { products } = useProducts();
-  const { status, error } = products;
+  const { productState } = useProducts();
+  const { status, products, error } = productState;
 
   return (
     <ul className={`product-list`}>
       {status === "loading" && <Loader />}
       {status === "succeeded" &&
-        products.products.map((product) => {
+        products.map((product) => {
           return <ProductCard key={product._id} product={product} />;
         })}
-      {status === "error" && <p>{error}</p>}
+      {status === "error" && notify(error, "error")}
     </ul>
   );
 };
